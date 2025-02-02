1d0e6cd4d537473cb5b45c658d5e265dc9629a16
zycro21 <dimaszycro10@gmail.com>
2025-02-03 02:08:49 +0700

    finish FE session for Admin

--- frontend_client/temudataku/src/pages/SessionDashboard.jsx
+++ frontend_client/temudataku/src/pages/SessionDashboard.jsx
@@ -166,6 +166,139 @@ const SessionDashboard = () => {
         }
     };
 
+    // State detail session
+    const [isModalDetailOpen, setIsModalDetailOpen] = useState(false);
+    const [selectedSession, setSelectedSession] = useState(null);
+
+    const handleDetailSessionClick = async (sessionId) => {
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                toast.error("Token tidak ditemukan!");
+                return;
+            }
+
+            const response = await axiosInstance.get(`http://localhost:8000/api/sessions/getSessionById/${sessionId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            setSelectedSession(response.data);  // Menyimpan data sesi ke dalam state
+            setIsModalDetailOpen(true);  // Membuka modal
+        } catch (error) {
+            toast.error("Gagal mengambil data sesi");
+        }
+    };
+
+    // State update session
+    const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+    const [updatedData, setUpdatedData] = useState({});
+
+    // Fungsi open modal update
+    const handleOpenUpdateModal = (session) => {
+        setSelectedSession(session);
+        setUpdatedData({
+            title: session.title,
+            description: session.description,
+            price: session.price,
+            duration: session.duration,
+            service_type: session.service_type,
+        });
+        setIsModalUpdateOpen(true);
+    };
+
+    // Fungsi handle perubahan input update
+    const handleUpdateInputChange = (e) => {
+        const { name, value } = e.target;
+        setUpdatedData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    // Fungsi Submit update
+    const handleUpdateSession = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                toast.error("Token tidak dapat ditemukan");
+                return;
+            }
+
+            // Filter data agar hanya mengirim yang diubah
+            const filteredData = Object.fromEntries(
+                Object.entries(updatedData).filter(([_, v]) => v !== "")
+            );
+
+            const response = await axiosInstance.put(`http://localhost:8000/api/sessions/updateSessionById/${selectedSession.session_id}`, filteredData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            toast.success(response.data.message);
+            setIsModalUpdateOpen(false);
+            fetchSessions(currentPage);
+        } catch (error) {
+            console.error("Error updating session:", error);
+            toast.error(error.response?.data?.message || "Terjadi kesalahan");
+        }
+    };
+
+    // Fungsi Hapus Session
+    const handleDeleteSession = async (sessionId) => {
+        const result = await Swal.fire({
+            title: "Anda yakin?",
+            text: "Session ini akan dihapus secara permanen dan tidak bisa dikembalikan lagi!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Ya, hapus!",
+            cancelButtonText: "Batal",
+        });
+
+        if (result.isConfirmed) {
+            try {
+                const token = localStorage.getItem("token");
+                const response = await axiosInstance.delete(`http://localhost:8000/api/sessions/deleteSessionById/${sessionId}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if (response.status === 200) {
+                    toast.success(response.data.message, {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        draggable: true,
+                        progress: undefined,
+                    })
+                }
+
+                fetchSessions(currentPage);
+            } catch (err) {
+                console.error("Error delete session: ", err);
+                toast.error(
+                    err.response?.data?.message ||
+                    "Error Ketika Proses Menghapus Session.",
+                    {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    }
+                );
+            }
+        }
+    }
+
     return (
         <>
             <div className={`user-dashboard-container ${!isOpen ? "sidebar-closed" : ""}`}>
@@ -253,24 +386,28 @@ const SessionDashboard = () => {
                                             <td>{session.session_id}</td>
                                             <td>{session.mentor_id}</td>
                                             <td>{session.title}</td>
-                                            <td>{session.total_price
-                                                ? `Rp${parseFloat(session.total_price).toLocaleString('id-ID')}`
-                                                : "-"}</td>
+                                            <td>{session.price
+                                                ? `Rp${parseFloat(session.price).toLocaleString('id-ID')}`
+                                                : "-"}
+                                            </td>
                                             <td>{session.duration} Minutes</td>
                                             <td>{session.service_type}</td>
                                             <td>
                                                 <button
                                                     className="action-button btn-detail"
+                                                    onClick={() => handleDetailSessionClick(session.session_id)}
                                                 >
                                                     <FontAwesomeIcon icon={faEye} />
                                                 </button>
                                                 <button
                                                     className="action-button btn-edit"
+                                                    onClick={() => handleOpenUpdateModal(session)}
                                                 >
                                                     <FontAwesomeIcon icon={faEdit} />
                                                 </button>
                                                 <button
                                                     className="action-button btn-delete"
+                                                    onClick={() => handleDeleteSession(session.session_id)}
                                                 >
                                                     <FontAwesomeIcon icon={faTrashAlt} />
                                                 </button>
@@ -371,6 +508,107 @@ const SessionDashboard = () => {
                         </div>
                     </div>
                 )}
+
+                {isModalDetailOpen && selectedSession && (
+                    <div className="detail-session-modal-overlay">
+                        <div className="detail-session-modal-content">
+                            <h2>Detail Session</h2>
+                            <div>
+                                <strong>Session ID:</strong> {selectedSession.session_id}
+                            </div>
+                            <div>
+                                <strong>Title:</strong> {selectedSession.title}
+                            </div>
+                            <div>
+                                <strong>Description:</strong> {selectedSession.description}
+                            </div>
+                            <div>
+                                <strong>Price:</strong> Rp{parseFloat(selectedSession.price).toLocaleString('id-ID')}
+                            </div>
+                            <div>
+                                <strong>Duration:</strong> {selectedSession.duration} Minutes
+                            </div>
+                            <div>
+                                <strong>Service Type:</strong> {selectedSession.service_type}
+                            </div>
+                            <div>
+                                <strong>Mentor Name:</strong> {selectedSession.mentor_name}
+                            </div>
+                            <div>
+                                <strong>Rating:</strong> {selectedSession.rating ? selectedSession.rating : "No rating yet"}
+                            </div>
+                            <button onClick={() => setIsModalDetailOpen(false)}>Close</button>
+                        </div>
+                    </div>
+                )}
+
+                {/* Modal Update */}
+                {isModalUpdateOpen && selectedSession && (
+                    <div className="update-session-modal-overlay">
+                        <div className="update-session-modal-content">
+                            <h2>Update Session</h2>
+                            <form onSubmit={handleUpdateSession}>
+                                <label htmlFor="title">Title</label>
+                                <input
+                                    type="text"
+                                    id="title"
+                                    name="title"
+                                    value={updatedData.title || ""}
+                                    onChange={handleUpdateInputChange}
+                                    placeholder="Enter session title"
+                                />
+
+                                <label htmlFor="description">Description</label>
+                                <textarea
+                                    id="description"
+                                    name="description"
+                                    value={updatedData.description || ""}
+                                    onChange={handleUpdateInputChange}
+                                    placeholder="Enter session description"
+                                />
+
+                                <label htmlFor="price">Price (Rp)</label>
+                                <input
+                                    type="number"
+                                    id="price"
+                                    name="price"
+                                    value={updatedData.price || ""}
+                                    onChange={handleUpdateInputChange}
+                                    placeholder="Enter price"
+                                />
+
+                                <label htmlFor="duration">Duration (Minutes)</label>
+                                <input
+                                    type="number"
+                                    id="duration"
+                                    name="duration"
+                                    value={updatedData.duration || ""}
+                                    onChange={handleUpdateInputChange}
+                                    placeholder="Enter duration in minutes"
+                                />
+
+                                <label htmlFor="service_type">Service Type</label>
+                                <select
+                                    id="service_type"
+                                    name="service_type"
+                                    value={updatedData.service_type || ""}
+                                    onChange={handleUpdateInputChange}
+                                >
+                                    <option value="">Select service type</option>
+                                    <option value="one_on_one">One-on-One</option>
+                                    <option value="group">Group</option>
+                                    <option value="bootcamp">Bootcamp</option>
+                                </select>
+
+                                <div className="update-sessions-modal-actions">
+                                    <button type="submit" className="btn-submit">Create</button>
+                                    <button type="button" className="btn-cancel" onClick={() => setIsModalUpdateOpen(false)}>Cancel</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                )}
+
 
             </div>
         </>
